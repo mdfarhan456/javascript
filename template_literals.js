d1c13b8fef5f86c0c8430a714372b3f4d785c35d
mdfarhan456 <farhanfarhan78165@gmail.com>
2025-01-12 22:47:59 +0530

    javascript

---
+++ template_literals.js
@@ -0,0 +1,14 @@
+function temp()
+{
+    // let text =`1ello javascript   // in tempalte literals (back tick) we can break line without using /n
+    // and i want to learn`
+    // console.log(text)
+
+    // let str = "and i want to learn javascript";
+    // let b = `thi is java ${str}`;
+    // console.log(b);   // in tempalte literals (back tick) we can concinate withou using + symbol
+
+    let str = `This is "js" and 'js' is good`;
+    alert(str);
+}
+temp()
