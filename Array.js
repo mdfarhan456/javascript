57be0f54e61706b95dbc03a7801617a584edcdd8
mdfarhan456 <farhanfarhan78165@gmail.com>
2025-01-13 22:32:48 +0530

    javascript

---
+++ Array.js
@@ -0,0 +1,13 @@
+function ananya()
+{
+    let arr = ["Mango","Apple","Banana","Graps"]  //Array
+   // console.log(arr.length) //4
+
+  // arr.pop()  //Remove last eliment
+
+   //arr.shift() //rmove last Eliment
+
+   arr.unshift("Orange");  // add first eliment
+   console.log(arr)
+
+}
