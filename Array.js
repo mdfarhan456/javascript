2746c36614db8e2ba155bd64f9faf5648a7c2004
mdfarhan456 <farhanfarhan78165@gmail.com>
2025-01-14 21:19:38 +0530

    javascript

--- Array.js
+++ Array.js
@@ -1,13 +1,36 @@
 function ananya()
 {
-    let arr = ["Mango","Apple","Banana","Graps"]  //Array
+    //let arr = ["Mango","Apple","Banana","Graps"]  //Array
+
+    //let arr2 = ["Orange"]
    // console.log(arr.length) //4
 
   // arr.pop()  //Remove last eliment
 
    //arr.shift() //rmove last Eliment
 
-   arr.unshift("Orange");  // add first eliment
-   console.log(arr)
+   //arr.unshift("Orange");  // add first eliment
+//    let arr3 = arr.concat(arr2)
+//    console.log(arr3)
+
+// let arr2 = arr.reverse()
+// console.log(arr2)
+
+// let arr2 =arr.slice(1,3)
+// console.log(arr2);
+
+//  arr.splice(2,0,"papaya")
+// console.log(arr)
+
+let arr =[7,8,1,5,8,2]
+
+let arr2 = arr.sort();
+ //console.log(arr2)
+
+
+arr2.reverse()
+console.log(arr2)
+
+
 
 }
