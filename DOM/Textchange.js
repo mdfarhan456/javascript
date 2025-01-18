2dafcd77b01b53801fa24049166d3aa89afc4f02
mdfarhan456 <farhanfarhan78165@gmail.com>
2025-01-18 18:31:19 +0530

    javascript

---
+++ DOM/Textchange.js
@@ -0,0 +1,20 @@
+function text()
+{
+    let change = document.querySelector("#change");
+    let hello =change.innerHTML="Hello cybrom";
+    change.style.color="red";
+    change.style.fontSize="50px";
+    
+    
+}
+
+
+function hero()
+{
+    document.body.style.backgroundColor="red";
+}
+
+function res()
+{
+    document.body.style.backgroundColor="white";
+}
