07236ff570711c46c4e013aeaef3f61bd60b5609
mdfarhan456 <farhanfarhan78165@gmail.com>
2025-01-26 21:18:39 +0530

    javascript

---
+++ function/task03.js
@@ -0,0 +1,41 @@
+let inp2=()=>{
+    let text=document.querySelector("#inp").value
+    //document.querySelector("#body")
+    document.body.style.backgroundColor=text
+
+}
+
+// let inp2=()=>{
+
+    
+//     if(document.body.style.backgroundColor=="white"){
+//         let text=document.querySelector("#go")
+//         text.style.color="white";
+//         document.body.style.backgroundColor="black";
+//     }
+//     else{
+//         let text=document.querySelector("#go")
+//         text.style.color="black";
+//         document.body.style.backgroundColor="white";
+//     }
+// }
+
+// let inp2=()=>{
+//     let box=document.querySelector("#div")
+//     //let box2=box.style.display="block"
+//     if(box.style.display=="block"){
+//         box.style.display="none"
+//         let text=document.querySelector("#but")
+//         text.innerHTML="show"
+//     }
+//     else{
+//         box.style.display="block"
+//         let text=document.querySelector("#but")
+//         text.innerHTML="hide"
+//     }
+    
+// }
+
+
+// 
+
